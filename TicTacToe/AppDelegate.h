d85d752549ef4099cae20df08a182ca303d7375e
Matthew Jeng <matthewjeng@Matthews-MBP.home>
2015-07-29 02:04:34 -0400

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Matthew Jeng on 7/29/15.
+//  Copyright (c) 2015 Matthew Jeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Matthew Jeng on 7/29/15.
+//  Copyright (c) 2015 Matthew Jeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
